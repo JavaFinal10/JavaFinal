10075a4e041f874833a78ca16e59e1eb4e3e8a18
Hodges <TH162922@davenportschools.org>
2018-10-10 09:47:53 -0500

    update

--- Java Final/src/main/Main.java
+++ Java Final/src/main/Main.java
@@ -1,10 +1,20 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			backStories.BackStory1.displayBackStory1();
+     int userChoice;		
+		
+     Scanner input = new Scanner(System.in);
+     
+     System.out.println("Before we start this story you will need to choose a number (this number will decide what charcter you are.)");
+     System.out.println("Choose a number between 1 and 5 >>> ");
+     System.out.println("");
+     
+	 backStories.BackStory3.displayBackStory3();		
 	}
 
 }
