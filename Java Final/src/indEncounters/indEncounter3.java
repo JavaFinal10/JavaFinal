2427d437d9dff528b67e617aea36ce40a96ed971
Jayden Graap <34245723+Graap02@users.noreply.github.com>
2018-10-18 09:05:24 -0500

    package update

---
+++ Java Final/src/indEncounters/indEncounter3.java
@@ -0,0 +1,16 @@
+package indEncounters;
+
+import java.util.Scanner;
+
+public class indEncounter3 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		int userChoice;
+		
+
+	}
+
+}
